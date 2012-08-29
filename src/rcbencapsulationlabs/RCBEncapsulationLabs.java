4eacc16a3db841d51658d302a7a17c21ca3ffd5e
rbath1 <rbath1@L11718038606.wctc.msft>
2012-08-29 18:14:27 -0500

    Original State

---
+++ src/rcbencapsulationlabs/RCBEncapsulationLabs.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rcbencapsulationlabs;
+
+/**
+ *
+ * @author rbath1
+ */
+public class RCBEncapsulationLabs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
