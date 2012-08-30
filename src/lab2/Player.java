1cc2285b405f1ad01f8e7e9ca85571579a861e2a
rbath1 <rbath1@L11718038606.wctc.msft>
2012-08-29 19:21:01 -0500

    alterations to lab02

---
+++ src/lab2/Player.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2;
+
+/**
+ *
+ * @author rbath1
+ */
+public class Player {
+    private String playerName;
+    
+    public Player(){
+    }
+    
+    public void setPlayerName(String playerName){
+        this.playerName = playerName;
+    }
+    public String getPlayerName(){
+        return playerName;
+    }
+}
